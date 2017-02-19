f3c5b24f377068a5b9d04402f5452f238dc48cc4
Ryan Stowell <pandamonium0@protonmail.com>
2017-02-18 20:21:40 -0600

    Remove unsupported syntax

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const { Clients, getClient } = require('./lib/clients');
-
-exports.Clients = Clients;
-exports.getClient = getClient;
 exports.connect = require('./lib/db').connect;
+exports.Clients = require('./lib/clients').Clients;
+exports.getClient = require('./lib/clients').getClient;
 
 exports.Document = require('./lib/document');
 exports.EmbeddedDocument = require('./lib/embedded-document');
